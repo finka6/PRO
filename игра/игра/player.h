be5935f944923163a36d7e9f5ff44f59be1f47ea
Elena <helen_tyz@mail.ru>
2018-12-21 01:00:20 +0300

    player ready

--- игра/игра/player.h
+++ игра/игра/player.h
@@ -4,6 +4,7 @@
 using namespace sf;
 
 
+
 class Player {
 private:
 	float x, y;
@@ -21,14 +22,16 @@ public:
 	float speed;
 	Clock clock;
 	float time;
+	int key;
+	int hp;
+	bool life;
 	void draw_p();
 	float CurrentFrame;
 	float coordinatePlayerX, coordinatePlayerY;
 	float getplayercoordinateY();
+	void Damage();
 	float getplayercoordinateX();
 	RectangleShape hpbar;
-	int key;
-	int hp;
-	bool life;
-
+	int win;
+	int ss;
 };
